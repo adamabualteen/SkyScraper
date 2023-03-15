6326b6ccd4469786ba51ad29afecd93c3f4350a4
Adam Appletini <abualteenya@gmail.com>
2023-03-14 23:49:36 -0400

    Added a whole bunch of stuff I cant even list them all ong

--- scripts.js
+++ scripts.js
@@ -1,4 +1,6 @@
-async function fetchCheapestFlight(departure_city, arrival_city, departure_date) {
+document.getElementById("currentYear").textContent = new Date().getFullYear();
+
+async function fetchCheapestFlight(departure_city, arrival_city, departure_date, travel_class) {
     const API_KEY = "34f2de9890msh7a926fcb0807e79p1ea45djsnb0bc9330aa86";
     const url = new URL("https://priceline-com-provider.p.rapidapi.com/v2/flight/departures");
     const params = {
@@ -7,6 +9,7 @@ async function fetchCheapestFlight(departure_city, arrival_city, departure_date)
         sid: "iSiX639",
         origin_airport_code: departure_city,
         destination_airport_code: arrival_city,
+        travel_class: travel_class,
     };
     url.search = new URLSearchParams(params);
 
@@ -32,6 +35,12 @@ async function validateRecaptcha() {
     return true;
 }
 
+async function displayResult(airline, price, departureCity, arrivalCity, departureDate, flightClass) {
+    const googleFlightsUrl = `https://www.google.com/flights?hl=en#flt=${departureCity}.${arrivalCity}.${departureDate}*${arrivalCity}.${departureCity}.*;c:USD;e:1;sd:1;t:f;tt:o;sp:.${flightClass}`;
+
+    document.getElementById("output").innerHTML = `The cheapest ${flightClass} class flight from ${departureCity} to ${arrivalCity} departing on ${departureDate} is $${price} on ${airline}. <a href="${googleFlightsUrl}" target="_blank">Click here to view the ticket</a>.`;
+}
+
 document.getElementById("flight-search-form").addEventListener("submit", async (event) => {
     event.preventDefault();
 
@@ -41,12 +50,11 @@ document.getElementById("flight-search-form").addEventListener("submit", async (
     const departure_city = document.getElementById("departure_city").value;
     const arrival_city = document.getElementById("arrival_city").value;
     const departure_date = document.getElementById("departure_date").value;
+    const travel_class = document.getElementById("travel_class").value;
 
-    const data = await fetchCheapestFlight(departure_city, arrival_city, departure_date);
+    const data = await fetchCheapestFlight(departure_city, arrival_city, departure_date, travel_class);
     const airline = data.getAirFlightDepartures.results.result.itinerary_data.itinerary_0.slice_data.slice_0.airline.name;
     const price = data.getAirFlightDepartures.results.result.itinerary_data.itinerary_0.price_details.baseline_total_fare;
-    const output = document.getElementById("output");
 
-    output.textContent = `The cheapest flight from ${departure_city} to ${arrival_city} departing on ${departure_date} is $${price} on ${airline}`;
+    await displayResult(airline, price, departure_city, arrival_city, departure_date, travel_class);
 });
-
