e2b483aa46a6b2ef1b4424b4c61ab7525c9283d3
Steven Truong <s.ttruong@yahoo.com>
2023-04-17 16:31:59 -0400

    Debugging

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 document.getElementById("currentYear").textContent = new Date().getFullYear();
 
-async function fetchCheapestFlight(departure_city, arrival_city, departure_date, travel_class) {
+async function fetchCheapestFlight(departure_city, arrival_city, departure_date) {
     const API_KEY = "34f2de9890msh7a926fcb0807e79p1ea45djsnb0bc9330aa86";
     const url = new URL("https://priceline-com-provider.p.rapidapi.com/v2/flight/departures");
     const params = {
@@ -9,7 +9,6 @@ async function fetchCheapestFlight(departure_city, arrival_city, departure_date,
         sid: "iSiX639",
         origin_airport_code: departure_city,
         destination_airport_code: arrival_city,
-        travel_class: travel_class,
     };
     url.search = new URLSearchParams(params);
 
@@ -35,12 +34,12 @@ async function validateRecaptcha() {
     return true;
 }
 
-function displayResult(airline, price, departureCity, arrivalCity, departureDate, flightClass) {
-    document.getElementById("output").textContent = `The cheapest ${flightClass} class flight from ${departureCity} to ${arrivalCity} departing on ${departureDate} is $${price} on ${airline}.`;
+function displayResult(airline, price, departureCity, arrivalCity, departure_date) {
+    document.getElementById("output").textContent = `The cheapest flight from ${departureCity} to ${arrivalCity} departing on ${departure_date} is $${price} on ${airline}.`;
 }
 
-function displayGoogleFlightsLink(departureCity, arrivalCity, departureDate, flightClass) {
-    const googleFlightsUrl = `https://www.google.com/flights?hl=en#flt=${departureCity}.${arrivalCity}.${departureDate}*${arrivalCity}.${departureCity}.*;c:USD;e:1;sd:1;t:f;tt:o;sp:.${flightClass}`;
+function displayGoogleFlightsLink(departureCity, arrivalCity, departure_date) {
+    const googleFlightsUrl = `https://www.google.com/flights?hl=en#flt=${departureCity}.${arrivalCity}.${departure_date}*${arrivalCity}.${departureCity}.*;c:USD;e:1;sd:1;t:f;tt:o;sp:.${flightClass}`;
     document.getElementById("google-flights-link").innerHTML = `<a href="${googleFlightsUrl}" target="_blank">Click here to search for the ticket on Google Flights</a>`;
 }
 
@@ -61,16 +60,21 @@ document.getElementById("flight-search-form").addEventListener("submit", async (
     const departure_city = document.getElementById("departure_city").value;
     const arrival_city = document.getElementById("arrival_city").value;
     const departure_date = document.getElementById("departure_date").value;
-    const travel_class = document.getElementById("travel_class").value;
+    //const travel_class = document.getElementById("travel_class").value;
 
     showLoadingMessage();
+    console.log("DONE WITH SHOW LOADING MESSAGE");
 
-    const data = await fetchCheapestFlight(departure_city, arrival_city, departure_date, travel_class);
+    const data = await fetchCheapestFlight(departure_city, arrival_city, departure_date);
+    console.log("DONE WITH FETCHING CHEAPEST FLIGHT");
     const airline = data.getAirFlightDepartures.results.result.itinerary_data.itinerary_0.slice_data.slice_0.airline.name;
+    console.log("DONE WITH GETAIRLINE");
     const price = data.getAirFlightDepartures.results.result.itinerary_data.itinerary_0.price_details.baseline_total_fare;
+    console.log("DONE WITH GETPRICE");
 
-    displayResult(airline, price, departure_city, arrival_city, departure_date, travel_class);
-    displayGoogleFlightsLink(departure_city, arrival_city, departure_date, travel_class);
+    displayResult(airline, price, departure_city, arrival_city, departure_date);
+    console.log("DONE WITH DISPLAYRESULT");
+    //displayGoogleFlightsLink(departure_city, arrival_city, departure_date);
 
     hideLoadingMessage();
 });
