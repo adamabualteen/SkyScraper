db765363c4e20f2f407b731c2421f3a152ee9daa
Steven Truong <s.ttruong@yahoo.com>
2023-04-17 17:42:13 -0400

    Fixed code and changed to transparent backgrounds

--- scripts.js
+++ scripts.js
@@ -1,5 +1,3 @@
-document.getElementById("currentYear").textContent = new Date().getFullYear();
-
 async function fetchCheapestFlight(departure_city, arrival_city, departure_date) {
     const API_KEY = "34f2de9890msh7a926fcb0807e79p1ea45djsnb0bc9330aa86";
     const url = new URL("https://priceline-com-provider.p.rapidapi.com/v2/flight/departures");
@@ -34,15 +32,6 @@ async function validateRecaptcha() {
     return true;
 }
 
-function displayResult(airline, price, departureCity, arrivalCity, departure_date) {
-    document.getElementById("output").textContent = `The cheapest flight from ${departureCity} to ${arrivalCity} departing on ${departure_date} is $${price} on ${airline}.`;
-}
-
-function displayGoogleFlightsLink(departureCity, arrivalCity, departure_date) {
-    const googleFlightsUrl = `https://www.google.com/flights?hl=en#flt=${departureCity}.${arrivalCity}.${departure_date}*${arrivalCity}.${departureCity}.*;c:USD;e:1;sd:1;t:f;tt:o;sp:.${flightClass}`;
-    document.getElementById("google-flights-link").innerHTML = `<a href="${googleFlightsUrl}" target="_blank">Click here to search for the ticket on Google Flights</a>`;
-}
-
 function showLoadingMessage() {
     document.getElementById("loading-message").textContent = "Finding flight...";
 }
@@ -60,21 +49,15 @@ document.getElementById("flight-search-form").addEventListener("submit", async (
     const departure_city = document.getElementById("departure_city").value;
     const arrival_city = document.getElementById("arrival_city").value;
     const departure_date = document.getElementById("departure_date").value;
-    //const travel_class = document.getElementById("travel_class").value;
 
     showLoadingMessage();
-    console.log("DONE WITH SHOW LOADING MESSAGE");
 
     const data = await fetchCheapestFlight(departure_city, arrival_city, departure_date);
-    console.log("DONE WITH FETCHING CHEAPEST FLIGHT");
     const airline = data.getAirFlightDepartures.results.result.itinerary_data.itinerary_0.slice_data.slice_0.airline.name;
-    console.log("DONE WITH GETAIRLINE");
     const price = data.getAirFlightDepartures.results.result.itinerary_data.itinerary_0.price_details.baseline_total_fare;
-    console.log("DONE WITH GETPRICE");
+    const output = document.getElementById("output");
 
-    displayResult(airline, price, departure_city, arrival_city, departure_date);
-    console.log("DONE WITH DISPLAYRESULT");
-    //displayGoogleFlightsLink(departure_city, arrival_city, departure_date);
-
+    output.textContent = `The cheapest flight from ${departure_city} to ${arrival_city} departing on ${departure_date} is $${price} on ${airline}`;
     hideLoadingMessage();
 });
+
