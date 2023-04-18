6512ebe234afd402df8290c291b562af616600bb
Ether64 <ethanccouch@gmail.com>
2023-04-18 15:58:41 -0400

    Handling user currency input
    Adding parameter to search function to capture user currency specification

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-async function fetchCheapestFlight(departure_city, arrival_city, departure_date, flight_class) {
+async function fetchCheapestFlight(departure_city, arrival_city, departure_date, flight_class, requested_currency) {
     const API_KEY = "34f2de9890msh7a926fcb0807e79p1ea45djsnb0bc9330aa86";
     const url = new URL("https://priceline-com-provider.p.rapidapi.com/v2/flight/departures");
     const params = {
@@ -7,7 +7,8 @@ async function fetchCheapestFlight(departure_city, arrival_city, departure_date,
         sid: "iSiX639",
         origin_airport_code: departure_city,
         destination_airport_code: arrival_city,
-        cabin_class: flight_class
+        cabin_class: flight_class,
+        requested_currency: requested_currency
     };
     url.search = new URLSearchParams(params);
 
@@ -62,12 +63,12 @@ document.getElementById("flight-search-form").addEventListener("submit", async (
 
     showLoadingMessage();
 
-    const data = await fetchCheapestFlight(departure_city, arrival_city, departure_date, flight_class);
+    const data = await fetchCheapestFlight(departure_city, arrival_city, departure_date, flight_class, requested_currency);
     const airline = data.getAirFlightDepartures.results.result.itinerary_data.itinerary_0.slice_data.slice_0.airline.name;
     const price = data.getAirFlightDepartures.results.result.itinerary_data.itinerary_0.price_details.baseline_total_fare;
     const output = document.getElementById("output");
 
-    output.textContent = `The cheapest flight from ${departure_city} to ${arrival_city} departing on ${departure_date}, under ${flight_class} Class is $${price} on ${airline}`;
+    output.textContent = `The cheapest flight from ${departure_city} to ${arrival_city} departing on ${departure_date}, under ${flight_class.toUpperCase()} Class is ${requested_currency}${price} on ${airline}`;
 
     displayGoogleFlightsLink(departure_city, arrival_city, departure_date, flight_class);
 
