b94903cde0c0184552d525c1dc195fb1000d8203
Ether64 <ethanccouch@gmail.com>
2023-04-18 14:06:59 -0400

    Added Flight_Class Variable
    Flight_class variable accounts for a user selection of Economy, Business, or First Class flights.

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-async function fetchCheapestFlight(departure_city, arrival_city, departure_date) {
+async function fetchCheapestFlight(departure_city, arrival_city, departure_date, flight_class) {
     const API_KEY = "34f2de9890msh7a926fcb0807e79p1ea45djsnb0bc9330aa86";
     const url = new URL("https://priceline-com-provider.p.rapidapi.com/v2/flight/departures");
     const params = {
@@ -7,6 +7,7 @@ async function fetchCheapestFlight(departure_city, arrival_city, departure_date)
         sid: "iSiX639",
         origin_airport_code: departure_city,
         destination_airport_code: arrival_city,
+        cabin_class: flight_class
     };
     url.search = new URLSearchParams(params);
 
@@ -55,17 +56,18 @@ document.getElementById("flight-search-form").addEventListener("submit", async (
     const departure_city = document.getElementById("departure_city").value;
     const arrival_city = document.getElementById("arrival_city").value;
     const departure_date = document.getElementById("departure_date").value;
+    const flight_class = document.getElementById("flight_class").value;
 
     showLoadingMessage();
 
-    const data = await fetchCheapestFlight(departure_city, arrival_city, departure_date);
+    const data = await fetchCheapestFlight(departure_city, arrival_city, departure_date, flight_class);
     const airline = data.getAirFlightDepartures.results.result.itinerary_data.itinerary_0.slice_data.slice_0.airline.name;
     const price = data.getAirFlightDepartures.results.result.itinerary_data.itinerary_0.price_details.baseline_total_fare;
     const output = document.getElementById("output");
 
-    output.textContent = `The cheapest flight from ${departure_city} to ${arrival_city} departing on ${departure_date} is $${price} on ${airline}`;
+    output.textContent = `The cheapest flight from ${departure_city} to ${arrival_city} departing on ${departure_date}, under ${flight_class} Class is $${price} on ${airline}`;
 
-    displayGoogleFlightsLink(departure_city, arrival_city, departure_date);
+    displayGoogleFlightsLink(departure_city, arrival_city, departure_date, flight_class);
 
     hideLoadingMessage();
     
